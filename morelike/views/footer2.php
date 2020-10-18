a5f08d63e08418e684f10c7dc87453eae8b72a2f
juanfco <jnfco_18@hotmail.com>
2020-10-18 16:12:16 -0300

    commit inicial

---
+++ morelike/views/footer2.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+  <footer class="footer" style="margin: 30px">
+    <div class="container" style="text-align:center; font-size:12px;">
+        <p class="muted credit">
+        Developed by <a href="#">Juan Francisco Abello Rodriguez y Máximo Rodrigo Hernandez Gonzalez</a></p>
+      </div>
+  </footer>
+</body>
+
+</html>
