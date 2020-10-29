9950103d7f7cfcdf3d5c6d6a28f8fb49b18e2595
juanfco <jnfco_18@hotmail.com>
2020-10-29 18:09:03 -0300

    mostrar registros tabla

--- morelike/views/newReport.php
+++ morelike/views/newReport.php
@@ -13,16 +13,26 @@
 			<!--se añaden las etiquetas de php que faltan -->
 			<?php foreach($usuarios as $row):?>
 				<tr>
-					
-                       
-                        <td><?=$row->nombre?></td>
-				
-					
+                    <td><?=$row->nombre?></td>
                     <td><?=$row->acceso?></td>
-					
 				</tr>
 			<?php endforeach;?>
 		</table>
-
+        <h3 class="text-center">Registros por día</h3>   
+        <table class="table table-striped">
+			<th>Fecha</th>
+            <th>Descripción</th>
+            <th>Ingreso</th>
+            <th>Egreso</th>
+			<!--se añaden las etiquetas de php que faltan -->
+			<?php foreach($data as $row):?>
+				<tr>
+                    <td><?=$row->fecha?></td>
+                    <td><?=$row->descripcion?></td>
+                    <td><?=$row->ingreso?></td>
+                    <td><?=$row->egreso?></td>
+				</tr>
+			<?php endforeach;?>
+		</table>    
 	</div>
 </div>
